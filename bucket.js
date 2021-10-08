a2818016ef0d188bfe0026848f733f7518fac44e
Anna <anna355y@stud.kea.dk>
2021-10-08 15:27:45 +0200

    added svg

--- bucket.js
+++ bucket.js
@@ -15,3 +15,30 @@ async function start() {
 function startManipulatingTheSvg() {
   console.log("svg loaded");
 }
+function startManipulatingTheSvg() {
+  const clickElm = document.querySelectorAll(".g_interact");
+  clickElm.forEach((el) => (el.style.fill = "grey"));
+  clickElm.forEach((el) => el.addEventListener("click", storeValue));
+  document.querySelectorAll(".color-btn").forEach((el) => el.addEventListener("click", choseColor));
+  //hover
+  document.querySelectorAll("path").forEach((el) => el.addEventListener("mouseover", hoverCap));
+  document.querySelectorAll("path").forEach((el) => el.addEventListener("mouseout", outCap));
+}
+function choseColor() {
+  selectedColor = this.getAttribute("fill");
+  document.querySelectorAll("path").forEach((el) => el.addEventListener("mouseover", hoverCap));
+  document.querySelectorAll("path").forEach((el) => el.addEventListener("mouseout", outCap));
+}
+
+function storeValue() {
+  this.style.fill = selectedColor;
+}
+
+function hoverCap() {
+  console.log(this);
+  this.style.filter = "brightness(140%)";
+}
+
+function outCap() {
+  this.style.filter = "brightness(100%)";
+}
