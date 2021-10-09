42367f27042c7cf407bb979d952d15e604994bf1
Anna <anna355y@stud.kea.dk>
2021-10-09 12:33:23 +0200

    color picker works + brightness on hover

--- bucket.js
+++ bucket.js
@@ -13,5 +13,30 @@ async function start() {
 }
 
 function startManipulatingTheSvg() {
-  console.log("svg loaded");
+  const clickElm = document.querySelectorAll(".g_interact");
+  clickElm.forEach((el) => (el.style.fill = "lightgray"));
+  clickElm.forEach((el) => el.addEventListener("click", storeValue));
+  document.querySelectorAll(".circle").forEach((el) => el.addEventListener("click", choseColor));
+  //hover
+  document.querySelectorAll("path").forEach((el) => el.addEventListener("mouseover", hoverCap));
+  document.querySelectorAll("path").forEach((el) => el.addEventListener("mouseout", outCap));
+}
+function choseColor() {
+  selectedColor = getComputedStyle(this).backgroundColor;
+  console.log(selectedColor);
+  document.querySelectorAll("path").forEach((el) => el.addEventListener("mouseover", hoverCap));
+  document.querySelectorAll("path").forEach((el) => el.addEventListener("mouseout", outCap));
+}
+
+function storeValue() {
+  this.style.fill = selectedColor;
+}
+
+function hoverCap() {
+  console.log(this);
+  this.style.filter = "brightness(110%)";
+}
+
+function outCap() {
+  this.style.filter = "brightness(100%)";
 }
