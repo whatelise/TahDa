58b8dcbc6693bfbd5f7a545eaf5722dff96c4327
Elise <aemccreary5@gmail.com>
2021-10-10 14:34:54 +0200

    something

--- bucket.js
+++ bucket.js
@@ -3,12 +3,24 @@
 let elementToPaint;
 let selectedColor;
 
+const features = {
+  dragon: false,
+  smiley: false,
+  snail: false,
+  pizza: false,
+  spiderweb: false,
+  PP: false,
+  panther: false,
+  money: false,
+};
+
 document.addEventListener("DOMContentLoaded", start);
 
 async function start() {
   let response = await fetch("hat.svg");
   let mySvgData = await response.text();
   document.querySelector(".product-image").innerHTML = mySvgData;
+  document.querySelectorAll(".option").forEach((option) => option.addEventListener("click", toggleOption));
   startManipulatingTheSvg();
 }
 
@@ -39,4 +51,58 @@ function hoverCap() {
 
 function outCap() {
   this.style.filter = "brightness(100%)";
+}
+
+///////////////////////////////////Configurator pt 2///////////////////////////////
+
+function toggleOption(event) {
+  const target = event.currentTarget;
+  const feature = target.dataset.feature;
+  console.log(feature);
+
+  // TODO: Toggle feature in "model"
+
+  // If feature is (now) turned on:
+  // - mark target as chosen (add class "chosen")
+  // - un-hide the feature-layer(s) in the #product-preview;
+  // - create featureElement and append to #selected ul
+  // - create FLIP-animation to animate featureElement from img in target, to
+  //   its intended position. Do it with normal animation or transition class!
+
+  // Else - if the feature (became) turned off:
+  // - no longer mark target as chosen
+  // - hide the feature-layer(s) in the #product-preview
+  // - find the existing featureElement in #selected ul
+  // - create FLIP-animation to animate featureElement to img in target
+  // - when animation is complete, remove featureElement from the DOM
+
+  if (features[feature]) {
+    // feature added
+    console.log(`Feature ${feature} is turned on!`);
+
+    // TODO: More code
+  } else {
+    // feature removed
+    console.log(`Feature ${feature} is turned off!`);
+
+    // TODO: More code
+  }
+}
+
+// Create featureElement to be appended to #selected ul - could have used a <template> instead
+function createFeatureElement(feature) {
+  const li = document.createElement("li");
+  li.dataset.feature = feature;
+
+  const img = document.createElement("img");
+  img.src = `images/feature_${feature}.png`;
+  img.alt = capitalize(feature);
+
+  li.append(img);
+
+  return li;
+}
+
+function capitalize(text) {
+  return text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
 }
