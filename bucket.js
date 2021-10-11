b877e8250d48d28c3245cb48d1e78ed4dfe90278
Anna <anna355y@stud.kea.dk>
2021-10-11 15:19:44 +0200

    patches placement

--- bucket.js
+++ bucket.js
@@ -19,9 +19,12 @@ document.addEventListener("DOMContentLoaded", start);
 async function start() {
   let response = await fetch("hat.svg");
   let mySvgData = await response.text();
+  let response2 = await fetch("hat-curve.svg");
+  let mySvgData2 = await response2.text();
   document.querySelector(".product-image").innerHTML = mySvgData;
   const newDiv = document.createElement("ul");
   newDiv.classList.add("patch-container");
+  /*  const hatCurve = document.querySelector(); */
   document.querySelector(".product-image").appendChild(newDiv);
   document.querySelectorAll(".option").forEach((option) => option.addEventListener("click", toggleOption));
   startManipulatingTheSvg();
